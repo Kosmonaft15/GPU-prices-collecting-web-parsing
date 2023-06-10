78279487f24c99ca84addaaf6f8aed9d32867c10
Kosmonaft15 <khrunoff@gmail.com>
2023-06-10 11:11:20 +0300

    Добавленны: хранение данных в файле, визуализация истории цен на графике, маштабирование и перемещение графика

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,172 @@
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace Сбор_цен_GPU
 {
     public partial class Form1 : Form
     {
+        //передвижение графика мышкой
+        private bool isLeftButtonPressed = false;
+        private Point mouseDown = Point.Empty;
+
         public Form1()
         {
             InitializeComponent();
+
+            //загрузка данных для графика
+            List<(string GPUname, DateOnly date, int pricemin, int pricemax)> GPU_price_data = new();
+            using (TextFieldParser parser = new TextFieldParser(@"d:\\result2.txt"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters("\t");
+                while (!parser.EndOfData)
+                {
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    GPU_price_data.Add((fields[0], DateOnly.Parse(fields[1]), int.Parse(fields[2]), int.Parse(fields[3])));
+                }
+            }
+
+            this.Text = "Загруженно: " + GPU_price_data.Count.ToString() + " записей";
+
+            //рисование графика
+            //создаем элемент Chart
+            Chart priceChart = new Chart();
+            //кладем его на форму и растягиваем на все окно.
+            priceChart.Parent = this;
+            priceChart.Dock = DockStyle.Fill;
+            //добавляем в Chart область для рисования графиков, их может быть
+            //много, поэтому даем ей имя.
+            priceChart.ChartAreas.Add(new ChartArea("Цены GPU"));
+            //Создаем и настраиваем набор точек для рисования графика, в том
+            //не забыв указать имя области на которой хотим отобразить этот
+            //набор точек.
+            Legend legend = new Legend("Legend1");
+            priceChart.Legends.Add(legend);
+            Series SeriesOfPointMin = new Series("4090 min");
+            Series SeriesOfPointMax = new Series("4090 max");
+            SeriesOfPointMin.ChartType = SeriesChartType.Spline;
+            SeriesOfPointMax.ChartType = SeriesChartType.Spline;
+            SeriesOfPointMin.YValuesPerPoint = 1;
+            SeriesOfPointMax.YValuesPerPoint = 1;
+            SeriesOfPointMin.ChartArea = "Цены GPU";
+            SeriesOfPointMax.ChartArea = "Цены GPU";
+            SeriesOfPointMin.Legend = "Legend1";
+            SeriesOfPointMax.Legend = "Legend1";
+            SeriesOfPointMin.Name = "min";
+            SeriesOfPointMax.Name = "max";
+            SeriesOfPointMin.Color = Color.Green;
+            SeriesOfPointMax.Color = Color.Red;
+
+            //mySeriesOfPoint.IsValueShownAsLabel = true;
+            //делает оси маштабируемыми
+            priceChart.ChartAreas["Цены GPU"].AxisX.ScaleView.Zoomable = true;
+            priceChart.ChartAreas["Цены GPU"].AxisY.ScaleView.Zoomable = true;
+            priceChart.Titles.Add("Цена 4090"); //заголовок графиков          
+            priceChart.MouseWheel += PriceChart_MouseWheel; //обработчик прокрутки мышки
+            priceChart.MouseMove += PriceChart_MouseMove;  //прицел курсора
+            priceChart.MouseUp += PriceChart_MouseUp;    //перемещение графика мышкой
+            priceChart.MouseDown += PriceChart_MouseDown;  //перемещение графика мышкой                         
+            
+            //набор точек для графиков
+            foreach (var item in GPU_price_data.Where(a => a.GPUname == "Nvidia RTX 4090"))
+            {
+                SeriesOfPointMin.Points.AddXY(item.date.ToDateTime(TimeOnly.MinValue), item.pricemin);
+                SeriesOfPointMax.Points.AddXY(item.date.ToDateTime(TimeOnly.MinValue), item.pricemax);
+            }
+
+            //Добавляем созданные наборы точек в Chart
+            priceChart.Series.Add(SeriesOfPointMin);
+            priceChart.Series.Add(SeriesOfPointMax);
+        }
+
+        private void PriceChart_MouseDown(object? sender, MouseEventArgs e)
+        //перемещение графика мышкой
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                isLeftButtonPressed = true;
+                mouseDown = e.Location;
+
+            }
+        }
+
+        private void PriceChart_MouseUp(object? sender, MouseEventArgs e)
+        //перемещение графика мышкой
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                isLeftButtonPressed = false;
+                mouseDown = Point.Empty;
+            }
+        }
+
+        private void PriceChart_MouseMove(object? sender, MouseEventArgs e)
+        //обработчик прицела курсора, перемещение графика мышкой
+        {
+            var chart = (Chart)sender;
+
+            //обработчик прицела курсора
+            {
+                Point mousePoint = new Point(e.X, e.Y);
+
+                chart.ChartAreas["Цены GPU"].CursorX.SetCursorPixelPosition(mousePoint, true);
+                chart.ChartAreas["Цены GPU"].CursorY.SetCursorPixelPosition(mousePoint, true);
+            }
+
+            //перемещение графика мышкой
+            {
+                if (isLeftButtonPressed)
+                {
+                    var result = chart.HitTest(e.X, e.Y);
+
+                    if (result.ChartElementType == (ChartElementType.PlottingArea))
+                    {
+                        var oldXValueX = result.ChartArea.AxisX.PixelPositionToValue(mouseDown.X);
+                        var newXValueX = result.ChartArea.AxisX.PixelPositionToValue(e.X);
+                        var oldXValueY = result.ChartArea.AxisY.PixelPositionToValue(mouseDown.Y);
+                        var newXValueY = result.ChartArea.AxisY.PixelPositionToValue(e.Y);
+
+                        chart.ChartAreas["Цены GPU"].AxisX.ScaleView.Position += oldXValueX - newXValueX;
+                        chart.ChartAreas["Цены GPU"].AxisY.ScaleView.Position += oldXValueY - newXValueY;
+                        mouseDown.X = e.X;
+                        mouseDown.Y = e.Y;
+                    }
+                }
+            }
+        }
+
+        private void PriceChart_MouseWheel(object? sender, MouseEventArgs e)
+        //обработчик зума
+        {
+            var chart = (Chart)sender;
+            var xAxis = chart.ChartAreas["Цены GPU"].AxisX;
+            var yAxis = chart.ChartAreas["Цены GPU"].AxisY;
+
+            try
+            {
+                if (e.Delta < 0) // Scrolled down.
+                {
+                    xAxis.ScaleView.ZoomReset();
+                    yAxis.ScaleView.ZoomReset();
+                }
+                else if (e.Delta > 0) // Scrolled up.
+                {
+                    var xMin = xAxis.ScaleView.ViewMinimum;
+                    var xMax = xAxis.ScaleView.ViewMaximum;
+                    var yMin = yAxis.ScaleView.ViewMinimum;
+                    var yMax = yAxis.ScaleView.ViewMaximum;
+                    var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 2.5;
+                    var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 2.5;
+                    var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 2.5;
+                    var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 2.5;
+                    xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                    yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using File = System.IO.File;
+using System.Net.Http;
 
 namespace Сбор_цен_GPU
 {
@@ -14,26 +15,170 @@ namespace Сбор_цен_GPU
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
-            Dictionary<string, List<List<string>>> result = Parsing(url: "https://www.xpert.ru/products.php?category_id=1103"); // GPU 4090
-
-            if (result != null)
+            Dictionary<string, List<List<string>>> result = new()
             {
-                //result["Nvidia RTX 4090"] = result["Nvidia RTX 4090"].OrderByDescending(a => a[2]).ToList();
-                foreach (var item in result)
+                { "Nvidia RTX 4090",    Parsing(url: "https://www.xpert.ru/products.php?category_id=1103") }, // GPU 4090
+                { "Nvidia RTX 4080",    Parsing(url: "https://www.xpert.ru/products.php?category_id=1111") }, // GPU 4080
+                { "Nvidia RTX 4070 Ti", Parsing(url: "https://www.xpert.ru/products.php?category_id=1114") }, // GPU 4070 Ti
+                { "Nvidia RTX 4060 Ti", Parsing(url: "https://www.xpert.ru/products.php?category_id=1128") }, // GPU 4060 Ti
+                { "Nvidia RTX 3090 Ti", Parsing(url: "https://www.xpert.ru/products.php?category_id=1073") }, // GPU 3090 Ti                
+                { "Nvidia RTX 3080 Ti", Parsing(url: "https://www.xpert.ru/products.php?category_id=1051") }, // GPU 3080 Ti
+                { "Nvidia RTX 3060",    Parsing(url: "https://www.xpert.ru/products.php?category_id=1042") }, // GPU 3060 
+            };
+
+            //сортировка по цене 
+            Dictionary<string, List<(string GPUname, int price)>> GPU_list_by_price = new();
+            //копирование словаря в новый с сортировкой по цене
+            if (result != null)
+            {                
+                foreach (var result_item in result)
                 {
-                    File.WriteAllText("d:\\4090.txt", item.Key + "\n\n");                    
-                    item.Value.ForEach(item => File.AppendAllText("d:\\4090.txt", string.Join("\t", item) + "\n"));
+                    if (result_item.Value.Count == 0)
+                    {
+                        GPU_list_by_price.Add(result_item.Key, value: null);
+                    }
+                    
+                    if (result_item.Value.Count > 0 
+                        && result.TryGetValue(result_item.Key, out var GPUarray))
+                    {
+                        var GPU_list = new List<(string GPUname, int price)>();
+                        GPUarray.RemoveAt(0); //удаление заголовка таблицы 
+                        foreach (var GPU_item in GPUarray)
+                        {
+                            if (int.Parse(GPU_item[2].Replace("руб.", "").Trim()) <= 300000)
+                            // товар с ценой в 5 раз большей, чем самый дорогой GPU, пропускается
+                            {
+                                GPU_list.Add((GPU_item[1].Trim(), int.Parse(GPU_item[2].Replace("руб.", "").Trim())));
+                            }
+                        }
+                        GPU_list_by_price.Add(result_item.Key, GPU_list.OrderBy(a => a.price).ToList());
+                    }
                 }                
             }
 
-            //Application.Run(new Form1());
+            //сохранение истории цен в файл
+            /*
+            if (GPU_list_by_price != null)
+            {
+                foreach (var item in GPU_list_by_price)
+                {
+                    File.AppendAllText("d:\\result1.txt", "\n" + DateTime.Today.ToShortDateString() + "\n"
+                                                              + item.Key + "\n\n");
+                    if (item.Value != null)
+                    {
+                        item.Value.ForEach(item => File.AppendAllText("d:\\result1.txt", string.Join("\t", item.GPUname, item.price) + "\n"));
+                    }
+                }
+            }
+            */
+
+            //подготовка массива для графика: GPU, дата, цена мин, цена макс
+            List<(string GPUname, DateOnly date, int pricemin, int pricemax)> GPU_price_data = new();
+            if (GPU_list_by_price != null)
+                foreach (var item in GPU_list_by_price)
+                {
+                    if (item.Value == null)
+                        GPU_price_data.Add((item.Key, DateOnly.FromDateTime(DateTime.Now), 0, 0));
+                    if (item.Value != null)
+                        GPU_price_data.Add((item.Key, DateOnly.FromDateTime(DateTime.Now), item.Value.Min(a => a.price), item.Value.Max(a => a.price)));
+                }
+
+            //сохранение данных для графика
+            if (GPU_price_data != null)
+            {
+                //сохранение цен в TXT 
+                foreach (var item in GPU_price_data)
+                {
+                   File.AppendAllText("d:\\result2.txt", string.Join("\t", item.GPUname, item.date, item.pricemin, item.pricemax) + "\n");                    
+                }
+
+                //сохранение цен в XML
+                /*
+                XmlSerializer serializer = new XmlSerializer(typeof(List<(string GPUname, DateOnly date, int pricemin, int pricemax)>));
+                using (var writer = new StreamWriter("d:\\result2.xml", true))
+                {
+                    serializer.Serialize(writer, GPU_price_data);                    
+                }
+                */
+
+                GPU_price_data.Clear();
+            }           
+            
+            if (result != null)
+            {
+                //result["Nvidia RTX 4080"] = result["Nvidia RTX 4080"].OrderBy(a => a[2]).ToList();
+
+                //сохранение цен в TXT
+                /*
+                foreach (var item in result)
+                {
+                    File.AppendAllText("d:\\result.txt", "\n" + DateTime.Today.ToShortDateString() + "\n"
+                                                              + item.Key + "\n\n");
+                    item.Value.ForEach(item => File.AppendAllText("d:\\result.txt", string.Join("\t", item) + "\n"));
+                }
+                */
+
+                //сохранение цен в XML
+                /*
+                XmlSerializer serializer = new XmlSerializer(typeof(List<List<string>>));
+                using (var writer = new StreamWriter("d:\\result.xml", true))
+                {
+                    Serialize(writer, result);
+                }
+                */
+
+                /*
+                foreach (var item in result)
+                {
+                    switch (item.Key)
+                    {
+                        case "Nvidia RTX 4090":
+                            File.WriteAllText("d:\\4090.txt", item.Key + "\n\n");
+                            item.Value.ForEach(item => File.AppendAllText("d:\\4090.txt", string.Join("\t", item) + "\n"));
+                            break;
+
+                        case "Nvidia RTX 4080":
+                            File.WriteAllText("d:\\4080.txt", item.Key + "\n\n");
+                            item.Value.ForEach(item => File.AppendAllText("d:\\4080.txt", string.Join("\t", item) + "\n"));
+                            break;
+
+                        case "Nvidia RTX 4070 Ti":
+                            File.WriteAllText("d:\\4070 Ti.txt", item.Key + "\n\n");
+                            item.Value.ForEach(item => File.AppendAllText("d:\\4070 Ti.txt", string.Join("\t", item) + "\n"));
+                            break;
+
+                        case "Nvidia RTX 3090 Ti":
+                            File.WriteAllText("d:\\3090 Ti.txt", item.Key + "\n\n");
+                            item.Value.ForEach(item => File.AppendAllText("d:\\3090 Ti.txt", string.Join("\t", item) + "\n"));
+                            break;
+
+                        case "Nvidia RTX 3090":
+                            File.WriteAllText("d:\\3090.txt", item.Key + "\n\n");
+                            item.Value.ForEach(item => File.AppendAllText("d:\\3090.txt", string.Join("\t", item) + "\n"));
+                            break;
+
+                        case "Nvidia RTX 3080 Ti":
+                            File.WriteAllText("d:\\3080 Ti.txt", item.Key + "\n\n");
+                            item.Value.ForEach(item => File.AppendAllText("d:\\3080 Ti.txt", string.Join("\t", item) + "\n"));
+                            break;
+
+                        case "Nvidia RTX 3080":
+                            File.WriteAllText("d:\\3080.txt", item.Key + "\n\n");
+                            item.Value.ForEach(item => File.AppendAllText("d:\\3080.txt", string.Join("\t", item) + "\n"));
+                            break;
+                    }
+                }*/
+            }                
+            
+            Application.Run(new Form1());
         }
 
-        private static Dictionary<string, List<List<string>>> Parsing(string url)
+        //парсинг xpert.ru
+        private static List<List<string>>? Parsing(string url)
         {
             try
             {
-                Dictionary<string, List<List<string>>> result = new Dictionary<string, List<List<string>>>();
+                List<List<string>> result = new List<List<string>>();
                 using (HttpClientHandler hdl = new HttpClientHandler())
                 {
                     using (var clnt = new HttpClient(hdl))
@@ -78,7 +223,7 @@ namespace Сбор_цен_GPU
 
                                             result_stroka = result_stroka.Skip(1).OrderBy(a => a[2]).ToList(); //сортировка по цене, пропуск заголовка, после сортировки он в конце
                                             result_stroka.Insert(0, title_row); //вставка заголовка таблицы                                            
-                                            result["Nvidia RTX 4090"] = result_stroka;
+                                            result = result_stroka;
                                         }                                        
                                     }                                    
 
@@ -95,6 +240,6 @@ namespace Сбор_цен_GPU
             }
 
             return null;
-        }
+        }     
     }
 }
